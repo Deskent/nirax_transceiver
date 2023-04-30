145513628c34ba458ea34c610c8e6cc3140de8cc
Andrew Deskent <battenetciz@gmail.com>
2023-04-30 13:16:48 +0300

    update swarm deploy files

--- app/routers.py
+++ app/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.src.views.api_transceiver import router as transceiver_router
+from src.views.api_transceiver import router as transceiver_router
 
 main_router = APIRouter(prefix="/api")
 main_router.include_router(transceiver_router)
