99e99913c1c5315c7ee2ef43d5d4b2b235c24e82
Andrew Deskent <battenetciz@gmail.com>
2023-04-30 14:28:30 +0300

    remove logs folder creating

--- swarm_restart.dev.sh
+++ swarm_restart.dev.sh
@@ -1,4 +1,4 @@
-docker stack rm nirax_transceiver \
+docker stack rm nirax_transceiver_dev \
 && sleep 2 \
-&& docker network rm nirax_transceiver_default -f
-export $(cat .swarm.dev.env) && docker stack deploy -c ./docker-swarm.dev.yml nirax_transceiver
+&& docker network rm nirax_transceiver_dev_default -f
+export $(cat .swarm.dev.env) && docker stack deploy -c ./docker-swarm.dev.yml nirax_transceiver_dev
