c138c6ebac5b6f572b25e2247863942e5a44ad00
Andrew Deskent <battenetciz@gmail.com>
2023-04-30 13:08:23 +0300

    update swarm deploy files

--- rollback_latest.dev.sh
+++ rollback_latest.dev.sh
@@ -1,1 +1,1 @@
-docker service update --image 94.103.93.84:5055/nirax/nirax_transceiver:latest dev_nirax_transceiver
+docker service update --image 94.103.93.84:5055/nirax/nirax_transceiver:latest nirax_transceiver_nirax_transceiver
--- swarm_restart.dev.sh
+++ swarm_restart.dev.sh
@@ -1,4 +1,4 @@
-docker stack rm dev \
+docker stack rm nirax_transceiver \
 && sleep 2 \
-&& docker network rm dev_default -f
-export $(cat .swarm.dev.env) && docker stack deploy -c ./docker-swarm.dev.yml dev
+&& docker network rm nirax_transceiver_default -f
+export $(cat .swarm.dev.env) && docker stack deploy -c ./docker-swarm.dev.yml nirax_transceiver
