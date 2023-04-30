a177b281b8086d938e434347e9d3c7f69cc4a21f
Andrew Deskent <battenetciz@gmail.com>
2023-04-30 13:45:35 +0300

    add tests and tox.ini

--- app/tests/test_aiohtttp_route.py
+++ app/tests/test_aiohtttp_route.py
@@ -1,6 +1,10 @@
+import pytest
 URL = '/aiohttp'
 
 
+@pytest.mark.github
+@pytest.mark.local
+@pytest.mark.server
 def test_aiohttp_route_ok(tclient, base_url, payload_aiohttp):
     response = tclient.post(base_url + URL, json=payload_aiohttp)
     assert response.status_code == 200
@@ -11,6 +15,9 @@ def test_aiohttp_route_ok(tclient, base_url, payload_aiohttp):
     assert data['result']
 
 
+@pytest.mark.github
+@pytest.mark.local
+@pytest.mark.server
 def test_aiohttp_route_bad_url(tclient, base_url, payload_aiohttp):
     payload_aiohttp['request_data']['url'] = 'blable'
     response = tclient.post(base_url + URL, json=payload_aiohttp)
@@ -21,6 +28,9 @@ def test_aiohttp_route_bad_url(tclient, base_url, payload_aiohttp):
     assert data['result'] is False
 
 
+@pytest.mark.github
+@pytest.mark.local
+@pytest.mark.server
 def test_requests_route_ok(tclient, base_url, payload_requests):
     response = tclient.post(base_url + URL, json=payload_requests)
     assert response.status_code == 200
@@ -31,6 +41,9 @@ def test_requests_route_ok(tclient, base_url, payload_requests):
     assert data['result']
 
 
+@pytest.mark.github
+@pytest.mark.local
+@pytest.mark.server
 def test_requests_route_bad_url(tclient, base_url, payload_requests):
     payload_requests['request_data']['url'] = 'blable'
     response = tclient.post(base_url + URL, json=payload_requests)
@@ -39,4 +52,3 @@ def test_requests_route_bad_url(tclient, base_url, payload_requests):
     assert data['message'].startswith('Invalid url')
     assert data['errors'] == []
     assert data['result'] is False
-
