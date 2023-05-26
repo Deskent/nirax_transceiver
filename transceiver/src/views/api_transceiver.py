6438c20ec5e549143c132e06d72f8a4a313976d0
deskent <battenetciz@gmail.com>
2023-05-26 09:05:48 +0300

    v 1.3.2: change log level

--- transceiver/src/views/api_transceiver.py
+++ transceiver/src/views/api_transceiver.py
@@ -15,10 +15,10 @@ router = APIRouter(prefix='/transceiver', tags=['Transceiver'], include_in_schem
     response_model=OutputSchema
 )
 async def resend(data: InputSchema):
-    logger.debug(data)
+    logger.info(data)
     try:
         results = await MainRequester(data).run_request()
-        logger.debug(results)
+        logger.info(results)
         return results
     except asyncio.exceptions.TimeoutError as err:
         logger.error(err)
