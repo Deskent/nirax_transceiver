c61fe7c9586ee1f890f670948134f5e4095e14a8
deskent <battenetciz@gmail.com>
2023-07-27 16:07:46 +0300

    v 1.3.14: add create_form_data param to InputSchema, add _create_form_data method for AsyncRequester, add update_payload_with_ssl

--- transceiver/src/classes/requesters/base_requester.py
+++ transceiver/src/classes/requesters/base_requester.py
@@ -2,17 +2,27 @@ from config import settings
 
 
 class BaseRequester:
-    """Base class for HTTP requests
+    """
+    Base class for HTTP requests.
 
         Attributes:
+            payload - Request params for sending
 
-            DEFAULT_TIMEOUT: int = 25 - Class attribute - Maximum request timeout
+            ssl_verify - SSL request param
+
+            timeout - Maximum request timeout
+
+            create_form_data - If True - will be created aiohttp.FormData before request
+                (Only for AsyncRequester class)
+        Methods
+            send_request
     """
 
-    def __init__(self, payload: dict, ssl_verify: bool = None):
+    def __init__(self, payload: dict, ssl_verify: bool = None, create_form_data: bool = False):
         self.payload: dict = payload
         self.ssl_verify: bool = ssl_verify
         self.timeout: int = self.payload.get('timeout', settings.DEFAULT_TIMEOUT)
+        self.create_form_data: bool = create_form_data
 
     async def send_request(self):
         raise NotImplementedError
